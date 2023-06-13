0e12218a786009ca8ba739bf4c27b6f5dc53ed84
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-06-13 15:26:49 -0300

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,34 +1,88 @@
+import Relogio from '../img/RelogioPNG.png'
+import Lixeira from '../img/Lixeira.png'
+
 const data = [
-  {
-    question: "O que é Vanilla JavaScript?",
-    options: [
-      "JavaScript puro",
-      "Uma biblioteca JavaScript",
-      "Um framework JavaScript",
-      "Um compilador de JavaScript",
+    {
+    category: "Revisão EJA",
+    questions: [
+      {
+        question: "O que consta numa reta numérica? ",
+        options: ["Letras", "Números", "Animais", "Cores"],
+        answer: "Números",
+      },
+      {
+        question: "Quais destes números são pares?",
+        options: ["3", "8", "5", "9"],
+        answer: "8",
+      },
+      {
+        question: "Quais destes números são ímpares? ",
+        options: ["7", "2", "4", "6"],
+        answer: "7",
+      },
+      {
+        question: "Qual parte da planta serve para sustentá-la?",
+        options: ["Folha", "Caule", "Raiz", "Fruto"],
+        answer: "Caule",
+      },
+      {
+        question: "Como se chama a parte da planta que suga os nutrientes?",
+        options: ["Folha", "Caule", "Raiz", "Flor"],
+        answer: "Raiz",
+      },
+      {
+        question: "Qual a cor da lixeira para reciclagem do vidro?",
+        options: ["Verde", "Amarelo", "Branco", "Azul"],
+        answer: "Verde",
+      },
+      {
+        question: "Quanto tempo para a decomposição do plástico? ",
+        options: ["45 anos", "300 anos", "450 anos", "100 anos"],
+        answer: "450 anos",
+      },
+      {
+        question: "Qual a cor da lixeira para reciclagem de papel e papelão?",
+        options: ["Amarelo", "Azul", "Verde", "Vermelha"],
+        answer: "Azul",
+      },
+      {
+        question: "Quantas sílabas tem a palavra CASAMENTO?  ",
+        options: ["3", "5", "6", "4"],
+        answer: "4",
+        tip: "CA | SA | MEN | TO"
+      },
+      {
+        question: "Qual a classificação de sílabas da palavra CASA? ",
+        options: ["Monossílaba", "Dissílaba", "Trissílaba", "Polissílaba"],
+        answer: "Dissílaba",
+      },
+      {
+        question: "O que o ponteiro grande marca no relógio?",
+        imagem: Relogio,
+        options: ["Horas", "Minutos", "Segundos", "Dias"],
+        answer: "Minutos",
+      },
+      {
+        question: "Um dia tem quantas horas?",
+        options: ["12h ", "24h", "15m", "60m"],
+        answer: "24h",
+      },
+      {
+        question: "Quantos minutos tem uma hora?",
+        options: ["15min", "35min", "60min", "12min"],
+        answer: "60min",
+      },
+      {
+        question: "Quantos meses constam no nosso calendário? ",
+        options: ["6 meses", "8 meses", "1 mês", "12 meses"],
+        answer: "12 meses",
+      },
+      {
+        question: "Quantos dias tem o mês de junho?",
+        options: ["31 dias", "30 dias", "28 dias", "29 dias"],
+        answer: "30 dias",
+      },
     ],
-    answer: "JavaScript puro",
-  },
-  {
-    question: "Com qual instrução declaramos uma constante em JavaScript?",
-    options: ["const", "let", "var", "define"],
-    answer: "const",
-  },
-  {
-    question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-    options: ["string", "number", "boolean", "float"],
-    answer: "float",
-  },
-  {
-    question: "Qual dos métodos a seguir seleciona um elemento?",
-    options: ["querySelector", "parseInt", "sort", "reduce"],
-    answer: "querySelector",
-  },
-  {
-    question:
-      "Qual destas propriedades da a quantidade de elementos de um array?",
-    options: ["qty", "length", "items", "index"],
-    answer: "length",
   },
 ];
 
