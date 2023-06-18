0b2a86f02ff9cc1c334d7c6b5bbbdce5496611fd
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-06-18 19:02:46 -0300

    Descrição das alterações feitas

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,11 +1,9 @@
-import Relogio from "../img/RelogioPNG.png";
-
 const data = [
   {
-    category: "Revisão EJA",
+    category: "JOGO DA REVISÃO",
     questions: [
       {
-        question: "O que consta numa reta numérica? ",
+        question: "O que consta numa reta numérica?",
         options: ["Letras", "Números", "Animais", "Cores"],
         answer: "Números",
       },
@@ -15,7 +13,7 @@ const data = [
         answer: "8",
       },
       {
-        question: "Quais destes números são ímpares? ",
+        question: "Quais destes números são ímpares?",
         options: ["7", "2", "4", "6"],
         answer: "7",
       },
@@ -25,11 +23,40 @@ const data = [
         answer: "150",
       },
       {
+        question: "Qual o sucessor (vem depois) do número 221?",
+        options: ["122", "62", "223", "103"],
+        answer: "222",
+      },
+      {
+        question: "O que o ponteiro grande marca no relógio?",
+        options: ["Horas", "Minutos", "Segundos", "Dias"],
+        answer: "Minutos",
+      },
+      {
+        question: "Um dia tem quantas horas?",
+        options: ["12h", "24h", "15m", "60m"],
+        answer: "24h",
+      },
+      {
+        question: "Quantos minutos tem uma hora?",
+        options: ["15min", "35min", "60min", "12min"],
+        answer: "60min",
+      },
+      {
+        question: "Quantos meses constam no nosso calendário?",
+        options: ["6 meses", "8 meses", "1 mês", "12 meses"],
+        answer: "12 meses",
+      },
+      {
+        question: "Quantos dias tem o mês de junho?",
+        options: ["31 dias", "30 dias", "28 dias", "29 dias"],
+        answer: "30 dias",
+      },
+      {
         question: "Complete a palavra com a sílaba faltante: BA __ NA",
         options: ["PE", "MA", "NA", "CO"],
         answer: "NA",
       },
-
       {
         question: "Complete a palavra com a sílaba faltante: MOS __ TO",
         options: ["QUI", "QUA", "LA", "TO"],
@@ -41,61 +68,14 @@ const data = [
         answer: "3",
       },
       {
-        question: "Qual o sucessor (vem depois) do número 221?",
-        options: ["122", "62", "222", "103"],
-        answer: "222",
-      },
-      {
-        question: "Qual a cor da lixeira para reciclagem do vidro?",
-        options: ["Verde", "Amarelo", "Branco", "Azul"],
-        answer: "Verde",
-      },
-      {
-        question: "Quanto tempo para a decomposição do plástico? ",
-        options: ["45 anos", "300 anos", "450 anos", "100 anos"],
-        answer: "450 anos",
-      },
-      {
-        question: "Qual a cor da lixeira para reciclagem de papel e papelão?",
-        options: ["Amarelo", "Azul", "Verde", "Vermelha"],
-        answer: "Azul",
-      },
-      {
-        question: "Quantas sílabas tem a palavra CASAMENTO?  ",
+        question: "Quantas sílabas tem a palavra CASAMENTO?",
         options: ["3", "5", "6", "4"],
         answer: "4",
-        tip: "CA | SA | MEN | TO",
       },
       {
-        question: "Qual a classificação de sílabas da palavra CASA? ",
+        question: "Qual a classificação de sílabas da palavra CASA?",
         options: ["Monossílaba", "Dissílaba", "Trissílaba", "Polissílaba"],
         answer: "Dissílaba",
-      },
-      {
-        question: "O que o ponteiro grande marca no relógio?",
-        imagem: Relogio,
-        options: ["Horas", "Minutos", "Segundos", "Dias"],
-        answer: "Minutos",
-      },
-      {
-        question: "Um dia tem quantas horas?",
-        options: ["12h ", "24h", "15m", "60m"],
-        answer: "24h",
-      },
-      {
-        question: "Quantos minutos tem uma hora?",
-        options: ["15min", "35min", "60min", "12min"],
-        answer: "60min",
-      },
-      {
-        question: "Quantos meses constam no nosso calendário? ",
-        options: ["6 meses", "8 meses", "1 mês", "12 meses"],
-        answer: "12 meses",
-      },
-      {
-        question: "Quantos dias tem o mês de junho?",
-        options: ["31 dias", "30 dias", "28 dias", "29 dias"],
-        answer: "30 dias",
       },
     ],
   },
